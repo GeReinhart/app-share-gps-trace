afa4ea7d9e5a6325d1a249e440fe944c0f8a5e36
Gerald Reinhart <gerald.reinhart@gmail.com>
2013-10-07 23:13:32 +0200

    rollback failing test

--- test/persistence_tests.dart
+++ test/persistence_tests.dart
@@ -74,7 +74,7 @@ main() {
       trail.title ="update title" ;
       return persitence.saveOrUpdateTrail(trail);
     }).then((savedTrail){
-      expect(savedTrail.title, "update title2") ;    
+      expect(savedTrail.title, "update title") ;    
     });
   }); 
 
