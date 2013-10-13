57007961a21f1914dad4a6aa9512fbe0e556d2d1
Gerald Reinhart <gerald.reinhart@gmail.com>
2013-10-13 16:07:20 +0200

    script for heroku deplyment

---
+++ prepareDeployHeroku.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -f web/*.js
+rm -f web/*.js.*
+
+dart2js web/index.dart -o web/index.dart.js
+
+git config --global user.email "support@drone.io"
+git config --global user.name "Drone Server"
+git status 
+
+git add web/index.dart.*
+git commit -m "add compiled files"
+
