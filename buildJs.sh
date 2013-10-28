631ebc3a41a1b29b162628593980b0b6207ae51b
Gerald Reinhart <gerald.reinhart@gmail.com>
2013-10-28 12:29:00 +0100

    add compiled js on Heroku

--- buildJs.sh
+++ buildJs.sh
@@ -5,6 +5,6 @@ rm -rf out
 mkdir out
 cp -r web/* out
 
-dart2js web/client/index.dart -o out/client/index.dart.js
+dart2js web/client/index.dart    -o out/client/index.dart.js
 dart2js web/client/register.dart -o out/client/register.dart.js
 
--- prepareDeployHeroku.sh
+++ prepareDeployHeroku.sh
@@ -3,13 +3,16 @@
 rm -f web/*.js
 rm -f web/*.js.*
 
-dart2js web/client/index.dart -o web/client/index.dart.js
+dart2js web/client/index.dart -o    web/client/index.dart.js
 dart2js web/client/register.dart -o web/client/register.dart.js
 
 git config --global user.email "support@drone.io"
 git config --global user.name "Drone Server"
 git status 
 
-git add web/client/index.dart.*
+git add web/client/*.dart.js
+git add web/client/*.dart.js.deps
+git add web/client/*.dart.js.map
+git add web/client/*.dart.precompiled.js
 git commit -m "add compiled files"
 
