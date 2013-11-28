4764184a1974df4a7f46e20290a69b5e2c321ae2
Gerald Reinhart <gerald.reinhart@gmail.com>
2013-11-29 00:28:57 +0100

    deployement fix

--- buildJs.sh
+++ buildJs.sh
@@ -9,3 +9,4 @@ dart2js web/client/index.dart    -o out/client/index.dart.js
 dart2js web/client/register.dart -o out/client/register.dart.js
 dart2js web/client/login.dart -o out/client/login.dart.js
 dart2js web/client/traceAnalysis.dart -o  out/client/traceAnalysis.dart.js
+dart2js web/client/traceAddForm.dart -o  out/client/traceAddForm.dart.js
--- test/model_tests.dart
+++ test/model_tests.dart
@@ -3,14 +3,7 @@ import '../lib/models.dart';
 
 main() {
   
-  test('Trail JSON serialization & deserialization', () {
-    Trail t = new Trail("Gex", "La classique de la Franche Verte", "franchVerte.gpx");
-    Map m = t.toJson();
-    Trail t2 = new Trail.fromJson(m);  
-    expect(t2.creator, equals(t.creator));    
-    expect(t2.title, equals(t.title));    
-    expect(t2.traceFile, equals(t.traceFile));    
-  });
+
 
   test('User JSON serialization & deserialization', () {
     User u = new User("Gex", "lakdsjgghkldsjag", "Gérald", "Reinhart");
