7ee77666541e48eead3ccb80997ac1a2d239eddb
GeReinhart <gerald.reinhart@gmail.com>
2013-12-24 15:43:02 +0100

    Update prepareDeployHeroku.sh

--- prepareDeployHeroku.sh
+++ prepareDeployHeroku.sh
@@ -6,18 +6,35 @@ dart test/test_runner.dart
 rm -f web/*.js
 rm -f web/*.js.*
 
-dart2js web/client/index.dart -o    web/client/index.dart.js
-dart2js web/client/register.dart -o web/client/register.dart.js
-dart2js web/client/login.dart -o web/client/login.dart.js
-dart2js web/client/traceAnalysis.dart -o  web/client/traceAnalysis.dart.js
-dart2js web/client/traceAddForm.dart -o  web/client/traceAddForm.dart.js
-dart2js web/client/traceSearch.dart -o  web/client/traceSearch.dart.js
-dart2js web/client/largeDisplay.dart -o  web/client/largeDisplay.dart.js
+
+
+prefixFile=`date +"%Y%m%d%H%M%S"`
+
+echo "prefix is $prefixFile"
+
+clientDir="web/client"
+clientFiles=`grep -r "main()" $clientDir   | grep -v packages |  awk -F":" '{print $1}' | awk -F"/" '{print $3}' `
+
+for clientFile  in $clientFiles
+do
+    cp "$clientDir/$clientFile" "$clientDir/$prefixFile-$clientFile"
+    dart2js "$clientDir/$prefixFile-$clientFile" -o "$clientDir/$prefixFile-$clientFile.js"
+done
+
+viewDir="web/rsp"
+viewFiles=`grep -r "type=\"application/dart\"" "$viewDir" | grep -v packages | awk -F":" '{print $1}' `
+
+for viewFile  in $viewFiles
+do
+   sed -i "s:src=\"/client/:src=\"/client/$prefixFile-:" "$viewFile"
+done
+
 
 git config --global user.email "support@drone.io"
 git config --global user.name "Drone Server"
 git status 
 
+git add "web/client/$prefixFile-*"
 git add web/client/*.dart.js
 git add web/client/*.dart.js.deps
 git add web/client/*.dart.js.map
