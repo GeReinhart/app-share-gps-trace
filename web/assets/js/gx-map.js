a9b422cc1fa9c530e44398f8ac7388cfb88c5aa1
GeReinhart <gerald.reinhart@gmail.com>
2014-04-04 14:31:01 +0200

    Update gx-map.js

--- web/assets/js/gx-map.js
+++ web/assets/js/gx-map.js
@@ -532,7 +532,7 @@ function GxTrace(key,  title, startLat, startLong, gpxUrl, icon,iconBuilder) {
      }
 
      this.zoomOnEndUserLocation = function(){
-        this.map.locate({ setView:true , enableHighAccuracy :  true, maxZoom : 11 }) ; 
+        this.map.locate({ setView:true , enableHighAccuracy :  true, maxZoom : 12 }) ; 
      }
 
      
